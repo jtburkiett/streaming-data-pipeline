30b0898139a56dc8a6500342099cd1d5df38b6e5
Hours with Experts <jtburkiett@gmail.com>
2023-04-03 19:12:16 -0500

    testing

--- scala-hello-world/src/main/scala/com/labs1904/hwe/App.scala
+++ scala-hello-world/src/main/scala/com/labs1904/hwe/App.scala
@@ -3,6 +3,7 @@ package com.labs1904.hwe
 object App {
   def main(args: Array[String]): Unit = {
     println("Hello World")
+    println(greeting("Joe"))
   }
 
   def greeting(name: String): String = {
